40606424751b145056ebf8a4bb306964bae061bb
david muhlberger <david.muhlberger@gmail.com>
2015-06-30 17:15:43 +0200

    changed sign up forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,32 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: :pages_controller?
 
   after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protected
+
+  def configure_permitted_parameters
+    # users
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+
+    # sellers
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :description_short
+    devise_parameter_sanitizer.for(:sign_up) << :description
+    devise_parameter_sanitizer.for(:sign_up) << :phone
+    devise_parameter_sanitizer.for(:sign_up) << :address_street
+    devise_parameter_sanitizer.for(:sign_up) << :address_zip
+    devise_parameter_sanitizer.for(:sign_up) << :address_city
+    devise_parameter_sanitizer.for(:sign_up) << :address_country
+
+  end
 
   private
 
