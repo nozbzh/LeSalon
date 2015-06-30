8578f9a6ff08f2dfd9e2688592d1c1cccdeaa8e7
noe stauffert <noe.stauffert@gmail.com>
2015-06-30 17:19:22 +0200

    seller show created

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,6 +1,7 @@
 class SellersController < ApplicationController
 
   def show
-
+    @seller = Seller.find(params[:id])
+    @seller_location = [{ lat: @seller.latitude, lng: @seller.longitude }]
   end
 end
