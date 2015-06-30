c9ad7265bd9bf7288099992394702673324faa21
Esther Banales <ebanalesm@gmail.com>
2015-06-30 14:25:02 +0200

    facebook connect added

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,12 @@
 #  address_street         :string
 #  address_zip            :string
 #  address_city           :string
+#  provider               :string
+#  uid                    :string
+#  picture                :string
+#  name                   :string
+#  token                  :string
+#  token_expiry           :datetime
 #
 # Indexes
 #
@@ -36,4 +42,19 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  devise :omniauthable, omniauth_providers: [:facebook]
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.name = auth.info.name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/omniauth_callbacks_controller'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
