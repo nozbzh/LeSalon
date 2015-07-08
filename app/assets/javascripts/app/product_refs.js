3bdf54ec1087b53382cbad1e8649ad2f5321b404
Esther Banales <ebanalesm@gmail.com>
2015-07-08 11:01:34 +0200

    finished page show

--- app/assets/javascripts/app/product_refs.js
+++ app/assets/javascripts/app/product_refs.js
@@ -1,6 +1,15 @@
 $(".sample-image").on("click", function() {
   $(".sample-image-active").removeClass("sample-image-active")
   $(this).addClass('sample-image-active')
-})
+});
 
+$(".product-thumbnail-img-link").click(function(e){
+  e.preventDefault();
+  var img_url = $(this).data("large-img");
+  $(".product-large-img").attr("src", img_url);
+});
 
+$(".show-thumbnails").on("click", function() {
+  $(".show-thumb-active").removeClass("show-thumb-active")
+  $(this).addClass('show-thumb-active')
+});
